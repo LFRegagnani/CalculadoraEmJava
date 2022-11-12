d3c836bac7e73288c85cfa8ea44aa8c7b8d501e2
Luiz Fernando Regagnani <luiz.fernando.regagnani@gmail.com>
2022-11-12 13:55:26 -0300

    Calculadora Simples feita em Java que pega os numeros na inicialização e faz diversas operações matematicas com eles

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        Calculadora calculadora = new Calculadora(73,62);
+        Calculadora calculadora7362 = new Calculadora(73,62);
 
 
-        calculadora.divideInvertido();
+        calculadora7362.divideInvertido();
+
+        calculadora7362.multiplica();
+
+        calculadora7362.soma();
 
 
     }
